c30609449eb89a6571374ea7450af91763c8ddab
Darwin Perdomo <ZeroBl@hotmail.com>
2022-12-08 15:10:38 -0400

    feat(api/): Change runtime field to a custom response

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -7,7 +7,7 @@ type Movie struct {
 	ID        int64     `json:"id"`
 	Title     string    `json:"title"`
 	Year      int32     `json:"year,omitempty"`
-	Runtime   int32     `json:"runtime,omitempty,string"`
+	Runtime   Runtime   `json:"runtime,omitempty"`
 	Genres    []string  `json:"genres,omitempty"`
 	CreatedAt time.Time `json:"-"`
 	Version   int32     `json:"version"`
