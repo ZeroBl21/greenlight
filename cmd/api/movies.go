f9ca752611d2102f4db90f24b2b3e67703f76b89
Darwin Perdomo <ZeroBl@hotmail.com>
2022-12-12 17:08:03 -0400

    feat(api/): Add json response with all data from the database

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -78,7 +78,16 @@ func (app *application) listMoviesHandler(w http.ResponseWriter, r *http.Request
 		return
 	}
 
-	fmt.Fprintf(w, "%+v\n", input)
+	movies, err := app.models.Movies.GetAll(input.Title, input.Genres, input.Filters)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"movies": movies}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
 
 // Returns the movie with the especified ID.
