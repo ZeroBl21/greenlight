7b8cd2b99fa3794d2c39709705a502e42a65d659
Darwin Perdomo <ZeroBl@hotmail.com>
2022-12-19 13:55:45 -0400

    feat(api/server): Add graceful shutdown to the backgrounds tasks

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -151,10 +151,14 @@ func (app *application) readInt(qs url.Values, key string, defaultValue int, v *
 
 // accepts any function with the signature func() as a parameter and stores it in
 // the variable fn. It then spins up a background goroutine, uses a deferred function
-// to recover any panics and log the error, and then executes the function itself 
+// to recover any panics and log the error, and then executes the function itself
 // by calling fn().
 func (app *application) background(fn func()) {
+	app.wg.Add(1)
+
 	go func() {
+		defer app.wg.Done()
+
 		defer func() {
 			if err := recover(); err != nil {
 				app.logger.PrintError(fmt.Errorf("%s", err), nil)
