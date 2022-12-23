636d1814834ada6202f2d90d80d35e73faebe1bf
Darwin Perdomo <ZeroBl@hotmail.com>
2022-12-23 13:48:16 -0400

    feat(data/permissions): Add Grant permissions to user method to the permissions model and user registration

--- internal/data/permissions.go
+++ internal/data/permissions.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"database/sql"
 	"time"
+
+	"github.com/lib/pq"
 )
 
 // Slice with hold the Permissions codes
@@ -60,4 +62,17 @@ func (m PermissionModel) GetAllForUser(userID int64) (Permissions, error) {
 	}
 
 	return permissions, nil
+}
+
+// Add the provided permissions codes for a specific user.
+func (m PermissionModel) AddForUser(userID int64, codes ...string) error {
+	query := `
+    INSERT INTO users_permissions
+    SELECT $1, permissions.id FROM permissions WHERE permissions.code = ANY($2)`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	_, err := m.DB.ExecContext(ctx, query, userID, pq.Array(codes))
+	return err
 }
