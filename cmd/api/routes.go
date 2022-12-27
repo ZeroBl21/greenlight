9d4c61e1891d36a41e5e6a2cf9c1c1b69e5fdc11
Darwin Perdomo <ZeroBl@hotmail.com>
2022-12-26 22:00:18 -0400

    feat(cmd/api): Add basic metric endpoint to the api

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"expvar"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
@@ -35,6 +36,9 @@ func (app *application) routes() http.Handler {
 	// Auth
 	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication",
 		app.createAuthenticationTokenHandler)
+
+	// Metrics
+	router.Handler(http.MethodGet, "/debug/vars", expvar.Handler())
 
 	return app.recoverPanic(app.enableCORS(app.rateLimit(app.authenticate(router))))
 }
